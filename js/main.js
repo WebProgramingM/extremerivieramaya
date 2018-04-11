38ed8d3db92d33f39dcca6c34614be1ab687fe9a
monzter50 <josemonzter50@gmail.com>
2018-04-11 11:38:13 -0500

    ultimos cambios

--- js/main.js
+++ js/main.js
@@ -38,4 +38,38 @@ $('a[href*="#"]')
       }
     }
   });
-  
+  $('.navbar-toggler').on('click', function() {
+        // $('#menuanim').toggleClass('open').stop();
+        $('#menuanim').each(function(i) { 
+            var elm=$(this);
+            setTimeout(function() { 
+                elm.toggleClass('open');
+            }, i * 250); 
+        }); 
+        $('nav').each(function(i) { 
+            var elm=$(this);
+            setTimeout(function() { 
+                elm.toggleClass('full');
+            }, i * 250); 
+        });
+    });
+$('.nav-link').on('click', function() {
+            $('nav').each(function(i) { 
+            var elm=$(this);
+            setTimeout(function() { 
+                elm.removeClass("full");;
+            }, i * 250); 
+        });
+        $('.navbar-collapse').each(function(i) { 
+            var elm=$(this);
+            setTimeout(function() { 
+                elm.removeClass("show");;
+            }, i * 250); 
+        });
+        $('#menuanim').each(function(i) { 
+            var elm=$(this);
+            setTimeout(function() { 
+                elm.removeClass("open");
+            }, i * 250); 
+        }); 
+    });
